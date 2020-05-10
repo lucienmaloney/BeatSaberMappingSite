b5397ee2b43dad659f6e052d7db9d4bd3dda0437
Lucien Maloney <hobbesdevelopment@gmail.com>
2020-05-10 08:49:12 -0500

    Progress

--- public/scripts/BeatSaber.js
+++ public/scripts/BeatSaber.js
@@ -1,26 +1,41 @@
 // JavaScript adaptation of https://github.com/lucienmaloney/BeatSaber.jl/blob/master/src/BeatSaber.jl
 
-function getpeaksfromaudio(data, bps) {
-  const audiorange = 1024;
-  // const spec =
-  // const len =
-  // const times =
-
-  // const flux =
-  const difference = [];
-  let rolling = 0;
-  const win = 20;
-
-  /*
-
-  */
-  const peaks = [[], [], [], [], []];
-  const ranges = [10, 7, 5, 3, 2];
-  for (let j = 0; j < 5; j++) {
-    r = ranges[j];
+function getflux(data, samplerate) {
+  const fft = new FFT(2048, samplerate);
+  const flux = [];
+  let spectrum = new Float64Array(1024);
+  for (let i = 0; i < data.length - 2048; i += 256) {
+    fft.forward(data.slice(i, i + 2048));
+    let x = 0;
+    for (let j = 0; j < 1024; j++) {
+      const diff = fft.spectrum[j] - spectrum[j];
+      x += diff > 0 ? diff : 0;
+    }
+    flux.push(x);
+    spectrum = fft.spectrum.slice(0, 1024);
   }
+  return flux;
+}
 
-  return peaks;
+function gettimestamps(data, samplerate) {
+  const notetimes = [];
+  const flux = getflux(data, samplerate);
+  const bound = 20;
+  let sum = flux.slice(0, bound).reduce((a, b) => a + b, 0);
+  let count = 5;
+
+  for (let i = 0; i < flux.length; i++) {
+    const lower = i - (bound + 1) >= 0 ? flux[i - (bound + 1)] : 0;
+    const upper = i + bound < flux.length ? flux[i + bound] : 0;
+    sum = sum - lower + upper;
+    count++;
+    if (flux[i] > (sum / (bound * 2 + 1) * 1.4 + 0.05) && count > 5) {
+      count = 0;
+      const time = 2 + (i * 256 / samplerate);
+      notetimes.push(time);
+    }
+  }
+  return notetimes;
 }
 
 function getnotedata(note, color = 0) {
@@ -54,20 +69,52 @@ function randbool() {
   return !Math.floor(Math.random() * 2)
 }
 
-function timestonotes(notes, threshold) {
+function weightedrandom(weights) {
+  const sum = weights.reduce((a, b) => a + b);
+  const rand = Math.random() * sum;
+  let rolling = 0;
+  for (let i = 0; i < weights.length; i++) {
+    rolling += weights[i];
+    if (rolling > rand) {
+      return i;
+    }
+  }
+  return 1;
+}
+
+function timestonotes(notetimes) {
   const notesb = [1, 1];
   const notesa = [13, 13];
   const notesequence = [];
-  const prevcolor = randbool();
+  let prevcolor = randbool();
 
   function pushnote(color, t, deltat) {
+    let weights = [];
+    for (let i = 0; i < 96; i++) {
+      weights[i] = notesallowed[i] *
+                   samecolor[notesb[0 + color] * 96 + i] *
+                   diffcolor[notesb[1 - color] * 96 + i] *
+                   samecolor2[notesa[0 + color] * 96 + i] *
+                   diffcolor2[notesa[1 - color] * 96 + i];
+    }
 
+    weights[notesb[1 - color]] *= 100;
+    const cap = deltat < 0.2 ? 2000 / (deltat ** 2) : 50000;
+    weights = weights.map(x => Math.min(x, cap));
+    const power = deltat > 0.2 ? 0.5 : 0.5 + 0.5 * (0.2 - deltat) / 0.2;
+
+    note = weightedrandom(weights.map(x => x ** power));
+
+    notesa[0 + color] = notesb[0 + color];
+    notesb[0 + color] = note;
+    prevcolor = color;
+    notesequence.push(createnotefromindex(note, 0 + color, t));
   }
 
   let t = 0;
   let timediff = 0;
 
-  for (let i = 0; i < notes.length; i++) {
+  for (let i = 0; i < notetimes.length; i++) {
     const newtime = notetimes[i];
     timediff = newtime - t;
     if (timediff < 0.2) {
@@ -84,28 +131,13 @@ function timestonotes(notes, threshold) {
   return notesequence;
 }
 
-function createbeatmapJSON(notes) {
+function createbeatmapJSON(notetimes) {
   return {
     "_version": "2.0.0",
     "_BPMChanges": [],
     "_events": [],
-    "_notes": notes,
+    "_notes": timestonotes(notetimes),
     "_obstacles": [],
     "_bookmarks": [],
   };
-}
-
-function createbeatmapJSONs(notetimes) {
-  const difficulties = [2, 1, 0.5, 0.3, 0.2];
-  return difficulties.map((d, i) => {
-    return createbeatmapJSON(timestonotes(notetimes[i], d));
-  });
-}
-
-function createmaps(data) {
-
-}
-
-function mapsong(data) {
-
 }
--- public/scripts/home.js
+++ public/scripts/home.js
@@ -4,25 +4,57 @@ const fileinput = document.getElementById("songupload");
 const reader = new FileReader();
 const audioctx = new AudioContext();
 
-function handlebuffer(buffer) {
-  const data = buffer.getChannelData(0);
-  for (let i = 1; i < buffer.numberOfChannels; i++) {
-    const newdata = buffer.getChannelData(i);
-    for (let j = 0; j < buffer.length; j++) {
-      data[j] += newdata[j];
-    }
-  }
-  const peaks = getpeaksfromaudio(data, buffer.sampleRate);
+function createmaps() {
+  const file = fileinput.files[0];
+  reader.readAsArrayBuffer(file);
 }
 
 reader.addEventListener("loadend", function(event) {
   let data = audioctx.decodeAudioData(event.target.result, handlebuffer);
 });
 
-function createmaps() {
-  const files = fileinput.files;
-  for (let i = 0; i < files.length; i++) {
-    const file = files[i];
-    reader.readAsArrayBuffer(file);
+function handlebuffer(buffer) {
+  console.log(buffer);
+  //encode(buffer, x => saveas(x, "newsong.ogg", "audio/ogg"));
+
+  const audio = buffer.getChannelData(0);
+  const length = audio.length;
+  for (let i = 1; i < buffer.numberOfChannels; i++) {
+    const audio2 = buffer.getChannelData(i);
+    for (let j = 0; j < length; j++) {
+      audio[j] = audio[j] + audio2[j];
+    }
   }
+  const timestamps = gettimestamps(audio, buffer.sampleRate);
+  const bmjson = createbeatmapJSON(timestamps);
+
+  const wavfile = audioBufferToWav(buffer);
+  const worker = new Worker('/scripts/worker/EmsWorkerProxy.js');
+
+  worker.onmessage = function(e) {
+    if (e.data && e.data.reply == "done") {
+      const name = fileinput.files[0].name.replace(/\..*/, "");
+      infojson["_songName"] = name;
+      const zip = new JSZip();
+      zip.file("info.dat", JSON.stringify(infojson));
+      zip.file("ExpertPlus.dat", JSON.stringify(bmjson));
+      zip.file("song.ogg", e.data.values["song.ogg"].blob);
+
+      zip.generateAsync({type:"blob"}).then(function(content) {
+        const randstring = Math.random().toString(36).substr(2);
+        saveas(content, `${randstring}_${name}.zip`, "application/zip");
+      });
+    }
+  }
+
+  worker.postMessage({
+    command: 'encode',
+    args: ["song.wav", "song.ogg"],
+    fileData: {"song.wav": new Uint8Array(wavfile)},
+    outData: {
+      'song.ogg': {
+        'MIME': 'audio/ogg',
+      },
+    },
+  });
 }
