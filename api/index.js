16a72e694296eb7d8abdb728994aa461abca8cf9
Aayush G <aayushg@mit.edu>
2022-04-19 21:06:40 +0200

    view folder

--- api/index.js
+++ api/index.js
@@ -4,14 +4,14 @@ const path = require("path");
 const port = process.env.PORT || 3000;
 const app = express();
 
-// app.set("view engine", "pug");
-// app.set("views", "../views");
-
-// app.use(express.static("public"));
-
-app.use("/public", express.static("public")); // serve static files in public subdirectory under /public virtual path
 app.set("view engine", "pug");
-app.set("views", path.join(__dirname, "../views")); // use pug templates file in /public/views subdirectory
+app.set("views", "../views");
+
+app.use(express.static("public"));
+
+// app.use("/public", express.static("public")); // serve static files in public subdirectory under /public virtual path
+// app.set("view engine", "pug");
+// app.set("views", path.join(__dirname, "../views")); // use pug templates file in /public/views subdirectory
 
 app.get("/", function (req, res) {
   res.status(200).render("home");
